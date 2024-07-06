ea37be956d9ac4e4e984396f803c1305a023772f
NanthuSSI <35912811+NanthuDev@users.noreply.github.com>
2024-07-06 09:57:38 +0530

    Update Employee.java

--- src/main/java/com/nklearning/demorest/model/Employee.java
+++ src/main/java/com/nklearning/demorest/model/Employee.java
@@ -1,8 +1,30 @@
 package com.nklearning.demorest.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name="employees")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name="first_name")
     private String firstName;
+
+    @Column(name="last_name")
     private  String lastName;
-    private String email;
+
+     private String email;
 }
