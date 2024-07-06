3fa568eb39a0a4ef7b99f2a1524cb972e0ee44e5
NanthuSSI <35912811+NanthuDev@users.noreply.github.com>
2024-07-06 09:25:31 +0530

    Create EmployeeRepository.java

---
+++ src/main/java/com/nklearning/demorest/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.nklearning.demorest.repository;
+
+import com.nklearning.demorest.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    //All Crud Operations
+    
+}
