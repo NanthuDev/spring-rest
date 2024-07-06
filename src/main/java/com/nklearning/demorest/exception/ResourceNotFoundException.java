3c88faa4466f99b8529fb6f3f296daaca756b18e
NanthuSSI <35912811+NanthuDev@users.noreply.github.com>
2024-07-06 09:57:27 +0530

    Update ResourceNotFoundException.java

--- src/main/java/com/nklearning/demorest/exception/ResourceNotFoundException.java
+++ src/main/java/com/nklearning/demorest/exception/ResourceNotFoundException.java
@@ -1,4 +1,12 @@
 package com.nklearning.demorest.exception;
 
-public class ResourceNotFoundException ex{
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends  RuntimeException{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
 }
